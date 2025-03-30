93f9136f60124326a70942f6aee5896ee9a40192
Jeffry Samuel <jeduarte30114@ufide.ac.cr>
2025-03-30 16:14:04 -0600

    Sistema para determinar que dias esta abierto

--- Proyecto/Controllers/CitaController.cs
+++ Proyecto/Controllers/CitaController.cs
@@ -27,7 +27,12 @@ public class CitaController : Controller
     {
         if (DateOnly.TryParse(date, out DateOnly parsedDate))
         {
+            List<string> AvailableDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
             List<string> AvailableTimes = ["7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM"];
+            
+            if (!AvailableDays.Contains(parsedDate.DayOfWeek.ToString()))
+                return Json(new { times = (List<string>) []}); 
+                       
             List<Cita> CitasExistentes = _context.Citas.Where(c => c.Fecha == parsedDate).ToList();
 
             foreach (Cita cita in CitasExistentes)
