7a88cd8e7f7b750e43528a936f4da16262cd3ab5
AlmightyBryan <bryan19251@gmail.com>
2025-04-19 19:44:19 -0600

    Vista de gestion de usuarios

---
+++ CoreLibrary/Models/ViewModels/ViewModel_UsuarioEditar.cs
@@ -0,0 +1,19 @@
+﻿using CoreLibrary.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.Models.ViewModels
+{
+    public class ViewModel_UsuarioEditar
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public Roles Rol { get; set; }
+    }
+}
